1a31d97c534dd1ac0f0d506fa00ca1fe438827b5
Quentin Santos <quentin.santos@worldonline.fr>
2017-06-03 09:39:16 +0200

    Just use mpz_powm with 2**t

--- square.c
+++ square.c
@@ -1,8 +1,8 @@
 #include <gmp.h>
 
 void square(mpz_t x, long t, mpz_t n) {
-    for (long i = 0; i < t; i += 1) {
-        mpz_mul(x, x, x);
-        mpz_mod(x, x, n);
-    }
+    mpz_t tmp;
+    mpz_init(tmp);
+    mpz_setbit(tmp, t);
+    mpz_powm(x, x, tmp, n);
 }
